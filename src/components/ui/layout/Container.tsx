595ca77b6e68fee370f90fc9624cf33a082e5a17
Eid Mohammad Pouya <empouya03@gmail.com>
2026-05-02 19:45:17 +0200

    fix: fix container polymorphic typing for production build, and global stylings

--- src/components/ui/layout/Container.tsx
+++ src/components/ui/layout/Container.tsx
@@ -1,17 +1,17 @@
-import { ReactNode } from "react";
+import { ElementType, ReactNode } from "react";
 
-type ContainerProps = {
+type ContainerProps<T extends ElementType = "div"> = {
     children: ReactNode;
     className?: string;
-    as?: keyof JSX.IntrinsicElements;
+    as?: T;
 };
 
-export default function Container({
+export default function Container<T extends ElementType = "div">({
     children,
     className = "",
-    as = "div",
-}: ContainerProps) {
-    const Component = as;
+    as,
+}: ContainerProps<T>) {
+    const Component = as || "div";
 
     return (
         <Component className={`mx-auto w-full max-w-6xl px-4 sm:px-6 lg:px-8 ${className}`}>
